61d3bc681638921bd7e9e7cfc7992f29e2c3b73a
c-mox <cmoxsh@gmail.com>
2021-02-02 18:02:07 +0600

    complete the project file coded

--- script.js
+++ script.js
@@ -1,16 +1,59 @@
-function getPin(){
+// Pin generated function:
+function getPin() {
     const random = Math.random() * 10000;
     const pin = (random + '').split('.')[0];
-    if(pin.length === 4){
+    if (pin.length === 4) {
         return pin;
-    }else{
+    } else {
         // console.log('Shoter pin', pin);
         return getPin();
     }
 }
 
-//Display generate pin
-function generatePin(){
-    const pinInput = document.getElementById('pn');
+//Display generated pin
+function generatePin() {
+    const pinInput = document.getElementById('pin');
     pinInput.value = getPin();
+}
+
+// handle calculator button event
+const buttonContainer = document.getElementById('digit-container');
+buttonContainer.addEventListener('click', function (event) {
+    const digit = event.target.innerText;
+    if (isNaN(digit)) {
+        if (digit === 'C') {
+            const typedInput = document.getElementById('typed-pin'); // handle clear for ('C')
+            typedInput.value = '';
+        }
+        else if (digit === 'B') {
+            const typedInput = document.getElementById('typed-pin'); // handle backspace for ('B')
+            typedInput.value = '';
+        } else {
+            // console.log();
+        }
+    } else {
+        const typedInput = document.getElementById('typed-pin');
+        typedInput.value = typedInput.value + digit;
+    }
+})
+
+// verify pin
+function verifyPin() {
+    const pin = document.getElementById('pin').value;
+    const typedPin = document.getElementById('typed-pin').value;
+    if (pin === typedPin) {
+        displayMatchResult('block', 'none');
+    }
+    else {
+        displayMatchResult('none', 'block');
+    }
+}
+
+// Match display result function
+function displayMatchResult(corrected, incorrected) {
+    const correct = document.getElementById('correct-pin');
+    correct.style.display = corrected;
+
+    const inCorrect = document.getElementById('incorrect-pin');
+    inCorrect.style.display = incorrected;
 }
